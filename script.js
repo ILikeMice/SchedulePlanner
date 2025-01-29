e4eba4b08e62dd554b11b98758c9482a4b6ef6bb
ILikeMice <ilikemice47@gmail.com>
2025-01-29 21:28:58 +0000

    (kind of) added dragging, not sure if i want to keep it so commiting for now

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function startresize(id) {
     let element = document.getElementById(id)
     const movelisten = (e) => {
         console.log(element.offsetTop)
-        element.style.height = e.clientY - element.getBoundingClientRect().top + "px" 
+        
+        element.style.height = e.clientY - element.getBoundingClientRect().top + 5 + "px" 
         
     }
     document.addEventListener("mousemove", movelisten)
@@ -16,3 +17,24 @@ function startresize(id) {
     
 }
 
+
+function startdrag(id) {
+    let element = document.getElementById(id)
+
+    element.style.cursor = "grabbing"
+
+    const movelisten = (e) => {
+        if (e.clientY - element.offsetHeight*2 < 0) {
+            return
+        }
+        element.style.top = e.clientY - element.offsetHeight*2 + "px"
+        console.log(element.style.top)
+    }
+
+    document.addEventListener("mousemove", movelisten)
+
+    document.addEventListener("mouseup", () => {
+        element.style.cursor = "grab"
+        document.removeEventListener("mousemove", movelisten)
+    })
+}
