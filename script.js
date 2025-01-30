d954c0c173793b2e42954504affb8bd95b113799
ILikeMice <ilikemice@posteo.de>
2025-01-30 15:39:08 +0000

    fixed some stuff, started working on adding events

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ let data = {
 
 function startresize(id) {
     let element = document.getElementById(id)
+
     const movelisten = (e) => {
         console.log(element.getBoundingClientRect().bottom - element.parentNode.getBoundingClientRect().top)
 
@@ -33,6 +34,8 @@ function startresize(id) {
         let hours = Math.floor(rawminutes / 60)
         let minutes = rawminutes % 60
         element.getElementsByClassName("righttime")[0].innerText = `${hours}:${minutes < 10 ? "0" + minutes : minutes}`
+        data[id.split("-")[0]][id.split("-")[1]]["end"] = rawminutes
+        
         try {
             document.getElementById(String(id.split("-")[0] + "-" + (Number(id.split("-")[1]) + 1))).getElementsByClassName("timespan")[0].innerText = `${hours}:${minutes < 10 ? "0" + minutes : minutes}`
         } catch {}
@@ -73,13 +76,34 @@ function startdrag(id) {
 
 function setscale() {
     let scale = document.getElementById("scaleinp").value
+    
     for (let i = 0; i < 7; i++) {
         let day = document.getElementsByClassName("day")[i]
         for (let b = 0; b < day.children.length; b++) {
             let node = day.children[b]
             console.log(i,b)
+
+            console.log(node.getElementsByClassName("timespan")[0].height * 2)
+
             node.style.minHeight = "unset"
             node.style.height = ((data[i + 1][b + 1]["end"] - data[i + 1][b + 1]["start"])*(day.offsetHeight / (1140/scale))) + "px"
+            node.style.overflow = "hidden"
+
+            node.getElementsByClassName("timespan")[0].innerText = data[i+1][b+1]["start"]
+            node.getElementsByClassName("righttime")[0].innerText = data[i+1][b+1]["end"]
         }
     }
+}
+
+function addevent(id) {
+    let daydiv = document.getElementById(id)
+    let eventdiv = document.createElement("div")
+    
+    let maxheight = 0
+    for (let i = 0; i < daydiv.children.length; i++) {
+        maxheight += daydiv.children[i].offsetHeight
+    }
+    eventdiv.style.height = daydiv.offsetHeight - maxheight
+    eventdiv.className = "event"
+    
 }
